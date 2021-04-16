d07c66779b6bba92192f908ed3fd55c5ea0d980b
ROUMAROU <rahina.oumaroumahamane@cnp.fr>
2021-04-16 11:29:55 +0200

    people covered by a specific station

--- src/main/java/com/safetynet/safetynetalerts/service/FirestationService.java
+++ src/main/java/com/safetynet/safetynetalerts/service/FirestationService.java
@@ -1,12 +1,21 @@
 package com.safetynet.safetynetalerts.service;
 
+import com.safetynet.safetynetalerts.dto.FireStationDTO;
+import com.safetynet.safetynetalerts.dto.PersonDTO;
 import com.safetynet.safetynetalerts.model.FireStation;
+import com.safetynet.safetynetalerts.model.MedicalRecord;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Data
@@ -60,5 +69,43 @@ public class FirestationService {
             log.error("Failed to delete the firestation");
         }
     }
+
+    public FireStationDTO getPersonsCoveredByFirestation(int stationNumber) {
+
+        log.debug("searching for addresses corresponding to stationNumber " + stationNumber);
+        List<FireStation> fireStations = jsonService.getAllFirestationsByStationNumber(stationNumber);
+        if (!fireStations.isEmpty()){
+            List<String> addresses = fireStations.stream().map(FireStation::getAddress).collect(Collectors.toList());
+
+            log.debug("searching for people covered by stationNumber " + stationNumber);
+            List<PersonDTO> personsByAddress = jsonService.getAllPersonsByAddress(addresses).stream()
+                    .map(person -> new PersonDTO(person.getFirstName(), person.getLastName(), person.getAddress(), person.getPhone()))
+                    .collect(Collectors.toList());
+
+            log.debug("Retrieving medical records");
+            List<MedicalRecord> medicalRecords = new ArrayList<>();
+            for (PersonDTO personDTO : personsByAddress){
+                medicalRecords.add(jsonService.getMedicalRecordByFirstNameAndLastName(personDTO.getFirstName(), personDTO.getLastName()));
+            }
+
+            log.debug("Getting information about age");
+            int adultNumber = (int) medicalRecords.stream().filter(mr -> calculateAge(mr.getBirthdate()) > 18).count();
+            int childrenNumber = (int) medicalRecords.stream().filter(mr -> calculateAge(mr.getBirthdate()) <= 18).count();
+
+            log.info("Person covered by stationNumber " + stationNumber + " successfully retrieved");
+            return new FireStationDTO(personsByAddress, adultNumber, childrenNumber);
+        }
+        log.error("No address corresponding to stationNumber " + stationNumber);
+
+        return null;
+    }
+
+    public int calculateAge(String birthDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        formatter = formatter.withLocale( Locale.FRANCE);
+        LocalDate date = LocalDate.parse(birthDate, formatter);
+        return Period.between(date, LocalDate.now()).getYears();
+    }
+
 
 }
--- src/main/java/com/safetynet/safetynetalerts/service/JSONService.java
+++ src/main/java/com/safetynet/safetynetalerts/service/JSONService.java
@@ -1,13 +1,19 @@
 package com.safetynet.safetynetalerts.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.safetynet.safetynetalerts.model.FireStation;
 import com.safetynet.safetynetalerts.model.JsonData;
+import com.safetynet.safetynetalerts.model.MedicalRecord;
+import com.safetynet.safetynetalerts.model.Person;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -26,6 +32,41 @@ public class JSONService {
             log.info(e.getMessage());
         }
 
+        return null;
+    }
+
+    public List<FireStation> getAllFirestationsByStationNumber(int stationNumber){
+        List<FireStation> fireStations = getDataFromJSONFile().getFirestations().stream().filter(f -> f.getStation() == stationNumber)
+                .collect(Collectors.toList());
+        if (!fireStations.isEmpty()){
+            log.info("Matching fire station(s) found");
+            return fireStations;
+        }
+        log.error("No matching fire station found!");
+        return List.of();
+    }
+
+    public List<Person> getAllPersonsByAddress(List<String> addresses){
+        List<Person> persons = getDataFromJSONFile().getPersons().stream().filter(p -> addresses.contains(p.getAddress()))
+                .collect(Collectors.toList());
+        if (!persons.isEmpty()){
+            log.info("Matching person(s) found");
+            return persons;
+        }
+        log.error("No match found!");
+        return null;
+    }
+
+    public MedicalRecord getMedicalRecordByFirstNameAndLastName(String firstName, String lastName){
+        Optional<MedicalRecord> medicalRecord = getDataFromJSONFile().getMedicalrecords().stream()
+                .filter(mr -> mr.getFirstName().equals(firstName) && mr.getLastName().equals(lastName))
+                .findAny();
+
+        if (medicalRecord.isPresent()){
+            log.info("Matching medical record found");
+            return medicalRecord.get();
+        }
+        log.error("Medical record not found!");
         return null;
     }
 
