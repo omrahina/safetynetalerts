ca2dfa9e6f91e492819ce167aeca849f797736fc
ROUMAROU <rahina.oumaroumahamane@cnp.fr>
2021-04-16 14:34:06 +0200

    MedicalRecord endpoint : CRUD

---
+++ src/main/java/com/safetynet/safetynetalerts/controller/MedicalRecordController.java
@@ -0,0 +1,36 @@
+package com.safetynet.safetynetalerts.controller;
+
+import com.safetynet.safetynetalerts.model.MedicalRecord;
+import com.safetynet.safetynetalerts.service.MedicalRecordService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/medicalRecord")
+public class MedicalRecordController {
+
+    private MedicalRecordService medicalRecordService;
+
+    public MedicalRecordController(MedicalRecordService medicalRecordService){
+        this.medicalRecordService = medicalRecordService;
+    }
+
+    @GetMapping
+    public Iterable<MedicalRecord> list(){
+        return medicalRecordService.list();
+    }
+
+    @PostMapping
+    public MedicalRecord addMedicalRecord(@RequestBody MedicalRecord medicalRecord){
+        return medicalRecordService.addMedicalRecord(medicalRecord);
+    }
+
+    @PutMapping
+    public MedicalRecord updateMedicalRecord(@RequestBody MedicalRecord medicalRecord){
+        return medicalRecordService.updateMedicalRecord(medicalRecord);
+    }
+
+    @DeleteMapping
+    public void deleteMedicalRecord(@RequestBody MedicalRecord medicalRecord){
+        medicalRecordService.deleteMedicalRecord(medicalRecord);
+    }
+}
