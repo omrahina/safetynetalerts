78cdefa4505544a079ca53c8463c70bcb4f1de96
ROUMAROU <crave627@gmail.com>
2021-04-19 04:37:23 +0200

    phoneAlert endpoint : service + tests

---
+++ src/main/java/com/safetynet/safetynetalerts/service/PhoneAlertService.java
@@ -0,0 +1,43 @@
+package com.safetynet.safetynetalerts.service;
+
+import com.safetynet.safetynetalerts.dto.PersonDTO;
+import com.safetynet.safetynetalerts.model.FireStation;
+import com.safetynet.safetynetalerts.model.Person;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+@Data
+public class PhoneAlertService {
+
+    private JSONService jsonService;
+
+    public PhoneAlertService(JSONService jsonService){
+        this.jsonService = jsonService;
+    }
+
+    public Iterable<String> getPhones(int firestation) {
+
+        log.debug("searching for addresses corresponding to firestation " + firestation);
+        List<FireStation> fireStations = jsonService.getAllFirestationsByStationNumber(firestation);
+        if (!fireStations.isEmpty()){
+            List<String> addresses = fireStations.stream().map(FireStation::getAddress).collect(Collectors.toList());
+            List<Person> personsByAddress = jsonService.getAllPersonsByAddress(addresses);
+            if (personsByAddress != null){
+                log.debug("Retrieving phone numbers of people covered by firestation " + firestation);
+                List<String> phones = personsByAddress.stream().map(Person::getPhone).collect(Collectors.toList());
+                log.info("Phone number(s) successfully retrieved!");
+                return phones;
+            }
+            log.error("No one found");
+            return null;
+        }
+        log.error("No address corresponding to firestation " + firestation);
+        return null;
+    }
+}
---
+++ src/test/java/com/safetynet/safetynetalerts/service/PhoneAlertServiceTest.java
@@ -0,0 +1,57 @@
+package com.safetynet.safetynetalerts.service;
+
+import com.safetynet.safetynetalerts.model.FireStation;
+import com.safetynet.safetynetalerts.model.Person;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class PhoneAlertServiceTest {
+
+    @Mock
+    JSONService jsonService;
+    @InjectMocks
+    PhoneAlertService phoneAlertService;
+
+    @Test
+    public void testGetPhones_success(){
+        FireStation fireStation = new FireStation("12 street", 1);
+        Person person = new Person("Yan", "Fan", "12 street",
+                "Paris", "75015", "01010101", "yan@gmail.com");
+        when(jsonService.getAllFirestationsByStationNumber(anyInt())).thenReturn(Collections.singletonList(fireStation));
+        when(jsonService.getAllPersonsByAddress(anyList())).thenReturn(Collections.singletonList(person));
+        Iterable<String> phones = phoneAlertService.getPhones(1);
+
+        assertThat(phones).isNotNull();
+        assertThat(phones).contains("01010101");
+    }
+
+    @Test
+    public void testGetPhones_unknownFirestation(){
+        when(jsonService.getAllFirestationsByStationNumber(anyInt())).thenReturn(List.of());
+        Iterable<String> phones = phoneAlertService.getPhones(1);
+
+        assertThat(phones).isNull();
+    }
+
+    @Test
+    public void testGetPhones_uninhabitedArea(){
+        FireStation fireStation = new FireStation("12 street", 1);
+        when(jsonService.getAllFirestationsByStationNumber(anyInt())).thenReturn(Collections.singletonList(fireStation));
+        when(jsonService.getAllPersonsByAddress(anyList())).thenReturn(null);
+        Iterable<String> phones = phoneAlertService.getPhones(1);
+
+        assertThat(phones).isNull();
+    }
+}
